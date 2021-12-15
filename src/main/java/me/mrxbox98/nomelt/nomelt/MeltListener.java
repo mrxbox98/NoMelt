e8d0b4ccca319a8071390d7a9a418df4984ed71d
Leenzery <63073463+LeenzeryDev@users.noreply.github.com>
2021-12-15 15:57:30 +0500

    fixed always burning fire

--- src/main/java/me/mrxbox98/nomelt/nomelt/MeltListener.java
+++ src/main/java/me/mrxbox98/nomelt/nomelt/MeltListener.java
@@ -1,5 +1,6 @@
 package me.mrxbox98.nomelt.nomelt;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFadeEvent;
@@ -9,7 +10,8 @@ public class MeltListener implements Listener {
     @EventHandler
     public void onBlockFade(BlockFadeEvent event)
     {
-        event.setCancelled(true);
+        if(event.getBlock().getType() != Material.FIRE){
+            event.setCancelled(true);
+        }
     }
-
 }
